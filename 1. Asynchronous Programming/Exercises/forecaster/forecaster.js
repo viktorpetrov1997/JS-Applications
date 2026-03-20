338b96c25b0b2ae0e8103d660e8e46eaa0dd43d3
Viktor Petrov <126717931+viktorpetrov1997@users.noreply.github.com>
2026-03-20 06:00:11 +0200

    Update exercise

--- 1. Asynchronous Programming/Exercises/forecaster/forecaster.js
+++ 1. Asynchronous Programming/Exercises/forecaster/forecaster.js
@@ -1,100 +1,105 @@
 function showForecast() 
 {
-    let conditions = 
+    let conditions =
     {
         'Sunny': '☀',
         'Partly sunny': '⛅',
         'Overcast': '☁',
         'Rain': '☂',
+        'Degrees': '°'
     };
- 
+
     const location = document.getElementById('location');
     const forecast = document.getElementById('forecast');
- 
+
     document.getElementById('submit').addEventListener('click', getWeather);
- 
+
     async function getWeather() 
     {
         try 
         {
             const url = `http://localhost:3030/jsonstore/forecaster/locations`;
- 
+
             const response = await fetch(url);
             const data = await response.json();
- 
+
             if(!response.ok) 
             {
                 throw new Error(`${response.status} (${response.statusText})`);
             }
- 
+
             const town = data.find(x => x.name.toLowerCase() == location.value.toLowerCase());
- 
+
             if(!town) 
             {
                 throw new Error('Error: Invalid town name')
             }
- 
+
             forecast.style.display = "block";
- 
+
             if(town) 
             {
                 getWeatherToday(town.code);
                 getWeatherUpcoming(town.code);
- 
+
                 location.value = '';
             }
-        } 
-        catch(error)
+        }
+        catch(error) 
         {
             forecast.style.display = "block";
             forecast.innerHTML = `<p>${error.message}</p>`
         }
     }
- 
+
     async function getWeatherToday(code) 
     {
         const url = `http://localhost:3030/jsonstore/forecaster/today/${code}`;
- 
+
         const response = await fetch(url);
         const data = await response.json();
- 
+
         const current = document.getElementById('current');
- 
-        let divCurr = document.createElement('div');
-        divCurr.setAttribute('class', 'forecasts');
-        divCurr.innerHTML = `
+
+        current.innerHTML = '';
+
+        const div = document.createElement('div');
+        div.setAttribute('class', 'forecasts');
+        div.innerHTML = `
           <span class="condition symbol">${conditions[data.forecast.condition]}</span>
           <span class="condition">
             <span class="forecast-data">${data.name}</span>
-            <span class="forecast-data">${data.forecast.low}°/${data.forecast.high}°</span>
+            <span class="forecast-data">${data.forecast.low}${conditions["Degrees"]}/${data.forecast.high}${conditions["Degrees"]}</span>
             <span class="forecast-data">${data.forecast.condition}</span>
           </span>   
         `
-        current.appendChild(divCurr);
+        current.appendChild(div);
     }
- 
+
     async function getWeatherUpcoming(code) 
     {
         const url = `http://localhost:3030/jsonstore/forecaster/upcoming/${code}`;
- 
+
         const response = await fetch(url);
         const data = await response.json();
- 
+
         const upcoming = document.getElementById('upcoming');
- 
-      const divForecastInfo = document.createElement('div');
-      divForecastInfo.setAttribute('class', 'forecast-info');
-      for (const x of data.forecast) 
-      {
-        divForecastInfo.innerHTML += `
-        <span class="upcoming">
-        <span class="symbol">${conditions[x.condition]}</span>
-        <span class="forecast-data">${x.low}°/${x.high}°</span>
-        <span class="forecast-data">${x.condition}</span>
-        </span>
-        `
-      }
-      upcoming.appendChild(divForecastInfo);
+
+        upcoming.innerHTML = '';
+
+        const div = document.createElement('div');
+        div.setAttribute('class', 'forecast-info');
+        for(const x of data.forecast) 
+        {
+            div.innerHTML += `
+            <span class="upcoming">
+            <span class="symbol">${conditions[x.condition]}</span>
+            <span class="forecast-data">${x.low}${conditions["Degrees"]}/${x.high}${conditions["Degrees"]}</span>
+            <span class="forecast-data">${x.condition}</span>
+            </span>
+            `
+        }
+        upcoming.appendChild(div);
     }
 }
 
